dab288433f5d2268efee0f57248c6803368a122d
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-18 20:16:41 +0900

    Setting

---
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbackng/SchoolmealBackNgApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cns.schoolmealbackng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmealBackNgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolmealBackNgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/dgsw/cns/schoolmealbackng/SchoolmealBackNgApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cns.schoolmealbackng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmealBackNgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
